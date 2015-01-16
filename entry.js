0f265f78703200cf9bf7fa570d48181840efa46e
David Jones <david@drj.io>
2015-01-16 16:14:11 -0500

    0000

--- entry.js
+++ entry.js
@@ -1,1 +1,16 @@
  var log = 0;
+var fs = require('fs');
+var base64data = "";
+var randomstring = require("randomstring");
+var n = randomstring.generate();
+
+var buff = new Buffer(base64data,'base64');
+console.log(base64data);
+
+var stream = fs.createWriteStream('./data/'+n+'.data');
+stream.write(buff);
+stream.on("end", function() {
+  stream.end();
+});
+
+
---
+++ runGit.sh
@@ -0,0 +1,5 @@
+cd /root/data-sauce
+node entry.js
+git add .
+git commit -m "0000"
+git push
